284b960811c78539b8432da342537b06e8c04745
Chandru <137380053+VKIDS2004@users.noreply.github.com>
2023-10-08 19:13:47 +0530

    nav-finish

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ let departmentPage = document.getElementById("departments-page-container");
 let aboutPage = document.getElementById("about-page-container");
 let contactPage = document.getElementById("contact-page-container");
 
-let homeBtn = document.getElementById("homeLink");
-let departmentBtn = document.getElementById("departmentLink");
-let aboutBtn = document.getElementById("aboutLink");
-let contactBtn = document.getElementById("contactLink");
+let homeBtn = document.getElementsByClassName("homeLink");
+let departmentBtn = document.getElementsByClassName("departmentLink");
+let aboutBtn = document.getElementsByClassName("aboutLink");
+let contactBtn = document.getElementsByClassName("contactLink");
+
+let responNav =document.getElementsByClassName("respon-nav")[0];
+let layoutModel =document.getElementsByClassName("layout_model")[0];
+
 
 function allHidecontainers(){
     homePage.classList.add("hidden");
@@ -16,50 +20,17 @@ function allHidecontainers(){
 }
 
 function removeAllActive(){
-    homeBtn.classList.contains("active-page")&&homeBtn.classList.remove("active-page");
-    departmentBtn.classList.contains("active-page")&&departmentBtn.classList.remove("active-page");
-    aboutBtn.classList.contains("active-page")&&aboutBtn.classList.remove("active-page");
-    contactBtn.classList.contains("active-page")&&contactBtn.classList.remove("active-page");
+for(let i =0;i<2;i++){
+    homeBtn[i].classList.contains("active-page")&&homeBtn[i].classList.remove("active-page");
+    departmentBtn[i].classList.contains("active-page")&&departmentBtn[i].classList.remove("active-page");
+    aboutBtn[i].classList.contains("active-page")&&aboutBtn[i].classList.remove("active-page");
+    contactBtn[i].classList.contains("active-page")&&contactBtn[i].classList.remove("active-page");
 }
-
-window.addEventListener("load",switchpage('Home'))
-
-
-function switchpage(action){
-    allHidecontainers();
-    removeAllActive();
-
-    switch (action) {
-        case "Home":
-            document.title = action;
-            homePage.classList.remove("hidden");
-            homeBtn.classList.add("active-page");
-            break;
-        case  "Department":
-            document.title = action;
-            departmentPage.classList.remove("hidden");
-            departmentBtn.classList.add("active-page")
-            break;
-        
-        case "About":
-            document.title = action;
-            aboutPage.classList.remove("hidden");
-            aboutBtn.classList.add("active-page");
-            break;
-        case "Contact":
-            document.title = "Contact"
-            contactPage.classList.remove("hidden");
-            contactBtn.classList.add("active-page");
-            break;
-    }
 }
-
-let a =document.getElementsByClassName("respon-nav")[0];
-let b =document.getElementsByClassName("layout_model")[0];
 
 function openModel(){
-    b.classList.add("open");
-    a.classList.add("open-nav");
+    layoutModel.classList.add("open");
+    responNav.classList.add("open-nav");
     document.addEventListener('keydown', function (e) {
         if (e.key === 'Escape' ) {
             closeModel();
@@ -69,13 +40,57 @@ function openModel(){
 }
 
 function closeModel(){
-    a.classList.remove("open-nav");
-    b.classList.remove("open");
-   
-
+    responNav.classList.remove("open-nav");
+    layoutModel.classList.remove("open");
 }
+
+window.addEventListener("load",switchpage('Home'))
+
+
+function switchpage(action){
+    allHidecontainers();
+    removeAllActive();
+
+    
+    switch (action) {
+        case "Home":
+            document.title = action;
+            homePage.classList.remove("hidden");
+            homeBtn[0].classList.add("active-page");
+            homeBtn[1].classList.add("active-page");
+            closeModel();
+            break;
+        case  "Department":
+            document.title = action;
+            departmentPage.classList.remove("hidden");
+            departmentBtn[0].classList.add("active-page");
+            departmentBtn[1].classList.add("active-page");
+            closeModel();
+            break;
+        
+        case "About":
+            document.title = action;
+            aboutPage.classList.remove("hidden");
+            aboutBtn[0].classList.add("active-page");
+            aboutBtn[1].classList.add("active-page");
+            closeModel();
+            break;
+        case "Contact":
+            document.title = "Contact"
+            contactPage.classList.remove("hidden");
+            contactBtn[0].classList.add("active-page");
+            contactBtn[1].classList.add("active-page");
+            closeModel();
+            break;
+        }
+        
+
+    
+}
+
+
 function nav_bar_res(){
-    if(a.classList.contains("open-nav")){
+    if(responNav.classList.contains("open-nav")){
        closeModel();
     }
     else{
